d02b156bc516d2df4f3729e8f41e7562bb1828d1
Jonas Jansen <git@jonasjansen.de>
2023-11-21 12:21:57 +0100

    Add structure for recommender by genre.

--- pages/Recommender_By_Genre.py
+++ pages/Recommender_By_Genre.py
@@ -6,4 +6,24 @@ st.set_page_config(
 )
 
 
-st.write("# Test")
+st.write("# Movie Recommendations by Genre")
+
+st.write("---")
+
+st.write("### Choose a Genre")
+
+option = st.selectbox(
+   "",
+   ("Action", "Comedy", "Horror"),
+   index=None,
+   placeholder="Select you Genre...",
+)
+
+# TODO: Remove. Only for debugging.
+#st.write("You selected:", option)
+
+st.write("---")
+
+st.write("### Your recommended movies")
+
+# TODO: Build a List of moves based on the selected genre.
