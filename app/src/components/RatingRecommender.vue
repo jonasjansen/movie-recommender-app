6a11f3599485df6b612c3d8fab1f8de55b9d1244
Jonas Jansen <git@jonasjansen.de>
2023-11-27 20:58:54 +0100

    Readd sample api request.

--- app/src/components/RatingRecommender.vue
+++ app/src/components/RatingRecommender.vue
@@ -1,13 +1,46 @@
 <template>
   <div>
     <h1>Recommender by Rating</h1>
+
+    <button @click="getMovieData">Get API Data</button>
+
+    <div v-if="movieData">
+      <p>{{ movieData }}</p>
+    </div>
+
+    <div v-if="error">
+      <p>Error fetching movie data: {{ error }}</p>
+    </div>
+
   </div>
 
 </template>
 
 <script>
+import axios from 'axios';
 export default {
-
+  data() {
+    return {
+      movieData: null,
+      error: null
+    };
+  },
+  methods: {
+    getMovieData() {
+      // Make a GET request to the /movie endpoint
+      axios.get('/api')
+          .then(response => {
+            // Handle successful response
+            this.movieData = response.data;
+            this.error = null;
+          })
+          .catch(error => {
+            // Handle error
+            this.error = error.message;
+            this.movieData = null;
+          });
+    }
+  }
 }
 
 </script>
