f00e1da62949d54e64dbb5314049731322190f9a
Noam Isachar <noam.isachar@gmail.com>
2023-11-30 19:13:32 +0200

    added implementations

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,19 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+import pandas as pd
+import numpy as np
+import random
+from math import recommend_by_genre, recommend_by_ratings
+
 
 app = Flask(__name__)
+
+similarity_matrix = np.load('similarity_matrix.npy')
+rating_matrix = pd.read_csv('rating_matrix.csv')
+exploded_movies = pd.read_csv('exploded_movies.csv')
+popular_movies_ids = exploded_movies[exploded_movies['hybrid_score']
+                                     > 0.3]['movie_id'].unique().tolist()
+genres = exploded_movies['genre'].unique().tolist()
+genre_recommendations = {}
 
 # Example data for illustration
 movie_data = [
@@ -9,40 +22,38 @@ movie_data = [
     # Add more movie data as needed
 ]
 
+
 @app.route('/movie/genres')
 def get_all_genres():
-    # Logic to fetch and return all available genres
-    genres = ["action", "comedy", "drama", "sci-fi", "horror"]
     return jsonify(genres)
+
 
 @app.route('/movie/sample')
 def get_random_movies():
-    # Logic to fetch and return a random sample of movies
-    # Replace this with your actual implementation
-    return jsonify(movie_data)
+    # We sample movies that have a hybrid score above a threshold because they have better chances that the user watched them
+    return jsonify(random.sample(popular_movies_ids, 20))
+
 
 @app.route('/recommendation/by_genre/<selected_genre>')
 def get_recommendation_by_genre(selected_genre):
-    # Logic to fetch and return a recommendation based on the given genre
-    # Replace this with your actual implementation
-    genre_recommendation = [movie for movie in movie_data if movie['genre'] == selected_genre]
-    genre_recommendation = sorted(genre_recommendation, key=lambda x: x['rating'], reverse=True)[:10]
-    return jsonify(genre_recommendation)
+    if selected_genre in genre_recommendations:
+        recommendations = genre_recommendations[selected_genre]
+    else:
+        recommendations = recommend_by_genre(exploded_movies, selected_genre)
+        genre_recommendations[selected_genre] = recommendations
+
+    return jsonify(recommendations)
+
 
 @app.route('/recommendation/by_rating', methods=['POST'])
 def get_recommendation_by_rating():
     # TODO: Proper process the post data.
     # TODO: Find out how to debug this.
 
-    # Logic to fetch and return a recommendation based on the list of rated movies
-    # Replace this with your actual implementation
-    # Example: You might expect a JSON payload with a list of rated movies
-    #rated_movies = request.get_json().get('rated_movies', [])
+    # new_ratings = request.json['new_ratings']
+    new_ratings = request.get_json().get('rated_movies', [])
+    return jsonify(recommend_by_ratings(new_ratings, similarity_matrix, rating_matrix, exploded_movies))
 
-    # Dummy logic to recommend movies based on ratings
-    #rated_movies_recommendation = sorted(movie_data, key=lambda x: x['rating'], reverse=True)[:10]
-    #return jsonify(rated_movies_recommendation)
-    return jsonify(movie_data)
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ backend/math.py
@@ -0,0 +1,77 @@
+import numpy as np
+
+
+def recommend_by_genre(exploded_movies, genre):
+    """
+    Recommends movies based on genre.
+    :param exploded_movies: DataFrame containing movie data
+    :param genre: Genre to filter movies
+    :return: List of recommended movie IDs
+    """
+    return exploded_movies[exploded_movies['genre'] == genre].nlargest(
+        10, 'hybrid_score')['movie_id'].values
+
+
+def recommend_by_ratings(new_ratings, similarity_matrix, rating_matrix, exploded_movies):
+    """
+    Recommends movies based on user ratings.
+    :param new_ratings: New user ratings
+    :param similarity_matrix: Matrix containing similarity values between movies
+    :param rating_matrix: Matrix containing movie ratings
+    :param exploded_movies: DataFrame containing movie data
+    :return: List of recommended movie names
+    """
+    unrated_indices = np.where(np.isnan(new_ratings))[0]
+    rated_indices = np.where(~np.isnan(new_ratings))[0]
+
+    predictions = []
+    for l in unrated_indices:
+        neighborhood = np.intersect1d(
+            np.where(~np.isnan(similarity_matrix[l, :])), rated_indices)
+        numerator = np.dot(
+            similarity_matrix[l, neighborhood], new_ratings[neighborhood])
+        denominator = np.sum(similarity_matrix[l, neighborhood])
+
+        if denominator != 0:
+            prediction = numerator / denominator
+            predictions.append((l, prediction))
+
+    predictions.sort(key=lambda x: x[1], reverse=True)
+    top_10_recommendations = [
+        movie for movie, prediction in predictions[:10] if not np.isnan(prediction)]
+    recommended_movies = rating_matrix.columns[top_10_recommendations].values
+
+    if len(top_10_recommendations) < 10:
+        genre_recommendations = complete_recommend_by_ratings(
+            new_ratings, exploded_movies, rating_matrix)
+        genre_recommendations = list(
+            set(genre_recommendations) - set(top_10_recommendations))
+        top_10_recommendations += genre_recommendations[:10 - len(
+            top_10_recommendations)]
+
+    return recommended_movies
+
+
+def complete_recommend_by_ratings(new_ratings, exploded_movies, rating_matrix):
+    """
+    Completes recommendations based on ratings.
+    :param new_ratings: New user ratings
+    :param exploded_movies: DataFrame containing movie data
+    :param rating_matrix: Matrix containing movie ratings
+    :return: List of recommended movie IDs
+    """
+    try:
+        highest_rated_movie_id = rating_matrix.columns[np.nanargmax(
+            new_ratings)]
+        genre_of_highest_rated_movie = exploded_movies.loc[exploded_movies['movie_id']
+                                                           == highest_rated_movie_id, 'genre'].values[0]
+        movies_same_genre = exploded_movies[exploded_movies['genre']
+                                            == genre_of_highest_rated_movie]
+        rated_indices = np.where(~np.isnan(new_ratings))[0]
+        rated_movie_ids = rating_matrix.columns[rated_indices].values
+        movies_same_genre = movies_same_genre[~movies_same_genre['movie_id'].isin(
+            rated_movie_ids)]
+        return movies_same_genre.nlargest(10, 'hybrid_score')['movie_id'].tolist()
+
+    except (ValueError, IndexError):
+        return exploded_movies.nlargest(10, 'hybrid_score')['movie_id'].tolist()
